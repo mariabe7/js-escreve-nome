7025596a45721e6972df7f0d5ddba017388431f2
mariabe7 <cordeiro.maria27@escola.pr.gov.br>
2024-09-05 14:09:57 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1000, 1000);
+  background("white");
+}
+
+function draw() {
+  stroke("blue")
+  fill("red");
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35)
+  }
+}
